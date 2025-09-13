2f47bd0e0412b96c426842244bf309cdabb9a93a
USMAN SER <109582955+usmanser71@users.noreply.github.com>
2025-09-14 02:12:32 +0500

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -7,15 +7,15 @@ cmd({
     react: "🔢",
     desc: "pair",
     category: "download",
-    use: '.pair +923096287432',
+    use: '.pair +923351300389',
     filename: __filename
 },
 async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
 try{
 
-if(!q) return await reply("*Example - :* .pair +923096287432")
-const pair = await fetchJson(`https://pair-bfou.onrender.com/code?number=${q}`)
-const done = `_*SILENT-SOBX-MD NUMBER PAIRED SUCCESSFULLY...✅*_`
+if(!q) return await reply("*Example - :* .pair +923351300389")
+const pair = await fetchJson(`https://paironrender.com/code?number=${q}`)
+const done = `_*USMAN-MD NUMBER PAIRED SUCCESSFULLY...✅*_`
 
 const pir = pair.code
 m.reply(`${pir}\n\n${done}`)
