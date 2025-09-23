e44d0b52e3fcf29d8f920050ea9db97cee316bb9
USMAN SER <109582955+usmanser71@users.noreply.github.com>
2025-09-23 11:25:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "USMAN-MD~vAsxTagD#0qRvTD9gX67TJoK0hpnoUKwfs0QFiD2jycFqHyrJUq8",
+    SESSION_ID: process.env.SESSION_ID || "USMAN-MD~aUsnXCyJ#98_lVemq-Ec3amm2ollPT45yB3jNCw0eXdVHRuADNCM",
     CAPTION: process.env.CAPTION || "POWERED BY USMAN-MD",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
