f5d89bc5ca5ac1036df0da7862a7c1ee1a9539b4
USMAN-SER <109582955+usmanser71@users.noreply.github.com>
2025-10-10 16:01:35 +0500

    Create start.js

---
+++ start.js
@@ -0,0 +1,59 @@
+//Created By Sadeesha Coder and Silent lover 432 don't copy without permission of owner
+
+const fs = require('fs');
+const path = require("path");
+const { File } = require("megajs");
+const AdmZip = require("adm-zip");
+const axios = require("axios");
+
+const PLUGINS_DIR = './plugins'; // Directory where plugins will be extracted
+const LIB_DIR = './lib';
+const ZIP_DIR = './'
+const auid = './auth_info_baileys'
+const silsb = './silentlover'
+const mydt = './my_data'
+
+async function downloadAndExtractZip() {
+  try {
+
+  const MEGA_ZIP_LINK = 'https://mega.nz/file/apQWxLZI#JExRnVtpgMDQi7x8bE6LOSuvyYjvAcDyfay3T62BC6U';
+  
+    if (!fs.existsSync(mydt)) {
+      fs.mkdirSync(mydt, { recursive: true });
+    }
+    if (!fs.existsSync(auid)) {
+      fs.mkdirSync(auid, { recursive: true });
+    }
+    if (!fs.existsSync(silsb)) {
+      fs.mkdirSync(silsb, { recursive: true });
+    }
+    if (!fs.existsSync(PLUGINS_DIR)) {
+      fs.mkdirSync(PLUGINS_DIR, { recursive: true });
+    }
+    if (!fs.existsSync(LIB_DIR)) {
+        fs.mkdirSync(LIB_DIR, { recursive: true });
+      }
+
+    console.log('FETCHING FILE FOR USMAN-MD 🚀❤');
+    
+    const file = File.fromURL(MEGA_ZIP_LINK);
+    const fileData = await file.downloadBuffer();
+
+    const tempZipPath = path.join(__dirname, 'temp.zip');
+    fs.writeFileSync(tempZipPath, fileData);
+    console.log(` USMAN-MD FILES DOWNLOADED SUCCESSFULLY ✅`);
+
+    const zip = new AdmZip(tempZipPath);
+    zip.extractAllTo(ZIP_DIR, true); 
+
+    console.log('USMAN-MD SUCCESSFULLY CONNECTED ENJOY ❤✅');
+
+    fs.unlinkSync(tempZipPath);
+    require("./index.js");
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+downloadAndExtractZip();
