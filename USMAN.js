2484560d38e9cd412e485582a609791574182fc2
USMAN-SER <109582955+usmanser71@users.noreply.github.com>
2025-10-13 12:46:57 +0500

    Update and rename USMAN-X to USMAN.js

--- USMAN.js
+++ USMAN.js
@@ -13,3 +13,9 @@
 #    * @whatsapp : 923097373710
 #    * @description : USMAN-MD ,A Multi-functional Whatsapp Bot Md.
 #    * @version 1.0.0
+<div align="center">
+<pre style="color: #00FF99; font-size: 14px; background-color: #000000; padding: 10px; border: 2px solid #00FF99;">
+
+ht<h2 align="center"><img src="https://i.postimg.cc/tRmw992W/USMAN-SER.jpg" width="800" style="border-radius:50%;" />
+
+</pre>
